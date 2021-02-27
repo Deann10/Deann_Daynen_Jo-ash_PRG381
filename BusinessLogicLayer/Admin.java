89438e9ff3b3e1431b817362a9c64e0c014593dd
Deann10 <70572144+Deann10@users.noreply.github.com>
2021-02-27 20:19:43 +0200

    Added fields and methods for Admin, Booking and Client

--- BusinessLogicLayer/Admin.java
+++ BusinessLogicLayer/Admin.java
@@ -1,5 +1,33 @@
 package BusinessLogicLayer;
 
+import java.util.Scanner;
+
 public class Admin {
     
+    String adminName = "Raheal";
+    String password = "1234";
+
+    public void Login(){
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print(" Enter admin name: ");
+            String _adminName = scanner.nextLine();
+
+            System.out.print(" Enter password: ");
+            String _password = scanner.nextLine();
+
+            if (adminName.equals(_adminName) && password.equals(_password)) {
+                System.out.println(" User successfully logged-in.. ");
+            } else {
+                System.out.println(" In valid userName of password ");
+            }
+        }
+    }
+
+    public void Logout(){
+        //To do
+    }
+
+    public void GetBookings(){
+        //To do
+    }
 }
--- BusinessLogicLayer/Booking.java
+++ BusinessLogicLayer/Booking.java
@@ -1,5 +1,39 @@
 package BusinessLogicLayer;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Booking {
-    
+        private int bookingID;
+        private Menu menu;
+        private Event event;
+        private Client client;
+        private Payment payment;
+
+        private static AtomicInteger ID_GENERATOR = new AtomicInteger(1000);//system generates id
+
+        public Booking(Menu _menu, Event _event, Client _client, Payment _payment ){
+            this.bookingID = ID_GENERATOR.getAndIncrement();
+            this.menu = _menu;
+            this.event = _event;
+            this.client = _client;
+            this.payment = _payment;
+
+        }
+
+        public Menu getMenu(){
+            return menu;
+        }
+
+        public Event getEvent(){
+            return event;
+        }
+
+        public Client getClient(){
+            return client;
+        }
+
+        public Payment getPayment(){
+            return payment;
+        }
+
 }
