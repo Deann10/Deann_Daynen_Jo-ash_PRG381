04969e3948e52c6f6a87266b75f0d31f16ead727
DaynenMoodley <79743903+DaynenMoodley@users.noreply.github.com>
2021-02-27 21:02:32 +0200

    prg381
    Co-Authored-By: Deann10 <70572144+Deann10@users.noreply.github.com>

--- BusinessLogicLayer/Booking.java
+++ BusinessLogicLayer/Booking.java
@@ -1,5 +1,41 @@
 package BusinessLogicLayer;
 
+import java.time.Duration;
+
 public class Booking {
+    int selection;
+    Scanner input = new Scanner(System.in);
+
+    /***************************************************/
+
+    System.out.println("Choose from these choices");
+    System.out.println("-------------------------\n");
+    System.out.println("Book an event  ");
+    System.out.println("check date availalibity ");
+    System.out.println("make a payment"); 
+    System.out.println("Close ");
+
+    selection = input.nextInt();
+    return selection;    
+
+  if (selection==1 ) { 
+   
+    String name  = JOptionPane.showInputDialog("Please enter the Name of the booking ")
+    String Event = JOptionPane.showInputDialog("Please enter type of event.")
+    
+
+  }
+    if( selection == 2) { 
+        date DateOfEvent = String name  = JOptionPane.showInputDialog("Date of booking ") ; 
+
+        date current = new date() ; 
+        long days = Duration.between(current, DateOfEvent) ; 
+        if (days <15)  { 
+            System.out.println("Date Not Available  ");
+        }
+        else {
+            System.out.println("Date is available ");
+        }
+    }
     
 }
--- BusinessLogicLayer/Menu.java
+++ BusinessLogicLayer/Menu.java
@@ -1,5 +1,20 @@
 package BusinessLogicLayer;
 
 public class Menu {
+    int selection;
+    Scanner input = new Scanner(System.in);
+
+    /***************************************************/
+
+    System.out.println("Choose from these choices");
+    System.out.println("-------------------------\n");
+    System.out.println("Make a booking ");
+    System.out.println("View all events that can be held");
+    System.out.println("View all items on the menu"); 
+    System.out.println("Close ");
+
+    selection = input.nextInt();
+    return selection;    
+}
     
 }
